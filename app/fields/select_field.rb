03998c84ec33eeff4e462dd3c430d45719180be9
Mariko <maripiyok@gmail.com>
2016-02-25 21:33:18 +0900

    [administrate] modify select box.

--- app/fields/select_field.rb
+++ app/fields/select_field.rb
@@ -4,4 +4,18 @@ class SelectField < Administrate::Field::Base
   def to_s
     data
   end
+
+  def choices
+    choices = options.fetch(:choices)
+    if options[:with_blank]
+      choices_with_blank = [ "" ] + choices
+      choices_with_blank.zip(choices_with_blank)
+    else
+      choices.zip(choices)
+    end
+  end
+
+  def selected_choice
+    data
+  end
 end
